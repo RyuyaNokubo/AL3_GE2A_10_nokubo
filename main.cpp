a8a10d477bd2b96df8c2baf0ae961dbc75a425fb
RyuyaNokubo <k021g1489@g.neec.ac.jp>
2022-04-11 17:37:45 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("GE2A_10_ノクボ_リュウヤ");
+	win->CreateGameWindow("GE2A_10_ノクボ_リュウヤ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
